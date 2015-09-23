55a3c52795d19e98897a6672ff9cc6b4bc411060
Snorre E. Brekke <snorre.brekke@spk.no>
2015-09-23 12:09:00 +0200

    POPKORN-178 - Oversetter JCommander exception-mesages til norsk.

--- src/main/java/no/spk/faktura/input/InvalidParameterException.java
+++ src/main/java/no/spk/faktura/input/InvalidParameterException.java
@@ -1,5 +1,12 @@
 package no.spk.faktura.input;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
 
@@ -12,8 +19,19 @@ public class InvalidParameterException extends RuntimeException {
 
     private final String usageString;
 
+    private static final List<Function<String, Optional<String>>> oversettere = Arrays.asList(
+            (s) -> oversett("^The following options? (?:is|are) required: (.+)$", "Følgende valg er påkrevd: %s", s),
+            (s) -> oversett("^Found the option (.+) multiple times$", "Fant valget %s flere ganger.", s),
+            (s) -> oversett("^Can only specify option (.+) once.$", "Kan bare angi %s én gang.", s),
+            (s) -> oversett("^Expected a value after parameter (.+)$", "Forventet en verdi etter %s.", s),
+            (s) -> oversett("^Expected (.+) values after (.+)$", "Forventet %s verdier etter %s.", s),
+            (s) -> oversett("^Invalid value for (.+) parameter. Allowed values:(.+)$", "Ugyldig verdi for %s. Lovlige verdier: %s.", s),
+            (s) -> oversett("^(.+): couldn't convert (.+) to an integer$", "%s: Kunne ikke konvertere %s til et heltall.", s),
+            (s) -> oversett("^Unknown option: (.+)$", "Ukjent valg: %s.", s)
+    );
+
     InvalidParameterException(String usageString, final ParameterException cause) {
-        super(cause.getMessage());
+        super(hentMelding(cause));
         this.usageString = usageString;
     }
 
@@ -23,5 +41,30 @@ public class InvalidParameterException extends RuntimeException {
      */
     public String usage() {
         return usageString;
+    }
+
+    private static String hentMelding(ParameterException cause){
+        final String opprinneligMelding = cause.getMessage();
+
+        return oversettere.stream()
+                .map((o) -> o.apply(opprinneligMelding))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findAny()
+                .orElseGet(() -> opprinneligMelding);
+    }
+
+    private static Optional<String> oversett(String regex, String oversattMelding, String melding) {
+        final Pattern pattern = Pattern.compile(regex);
+        final Matcher matcher = pattern.matcher(melding);
+        if (matcher.matches()) {
+            final int grupper = matcher.groupCount();
+            String[] gruppeMatch = new String[grupper];
+            for (int i = 0; i < grupper ; i++) {
+                gruppeMatch[i] = matcher.group(i + 1);
+            }
+            return Optional.of(String.format(oversattMelding, gruppeMatch));
+        }
+        return Optional.empty();
     }
 }
---
+++ src/test/java/no/spk/faktura/input/InvalidParameterExceptionTest.java
@@ -0,0 +1,101 @@
+package no.spk.faktura.input;
+
+
+import java.nio.file.Path;
+import java.util.Optional;
+
+import com.beust.jcommander.Parameter;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * Tester oversetting av JCommander feilmeldinger.
+ * @author Snorre E. Brekke - Computas
+ */
+public class InvalidParameterExceptionTest {
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    ProgramArgumentsFactory<?> factory;
+
+    @Before
+    public void setUp() throws Exception {
+        factory = new ProgramArgumentsFactory<>(ArgumentsTest.class);
+    }
+
+    @Test
+    public void testOversettPaakrevd() throws Exception {
+        exception.expectMessage("Følgende valg er påkrevd: -r");
+        factory.create();
+    }
+
+    @Test
+    public void testGjentatteValg() throws Exception {
+        exception.expectMessage("Kan bare angi -r én gang.");
+        factory.create("-r", "abc", "-r", "abc");
+    }
+
+    @Test
+    public void testFeilEnum() throws Exception {
+        exception.expectMessage("Ugyldig verdi for -e. Lovlige verdier: [A, B].");
+        factory.create("-r", "abc", "-e", "X");
+    }
+
+    @Test
+    public void testForeventetVerdi() throws Exception {
+        exception.expectMessage("Forventet en verdi etter -r.");
+        factory.create("-r");
+    }
+
+    @Test
+    public void testFlereVerdierForEnParameterMangler() throws Exception {
+        exception.expectMessage("Forventet 2 verdier etter -m.");
+        factory.create("-r", "abc", "-m", "x");
+    }
+
+    @Test
+    public void testFeilIntegerVerdi() throws Exception {
+        exception.expectMessage("\"-o\": Kunne ikke konvertere \"x\" til et heltall.");
+        factory.create("-r", "abc", "-o", "x");
+    }
+
+    @Test
+    public void testUkjentValg() throws Exception {
+        exception.expectMessage("Ukjent valg: -u");
+        factory.create("-u");
+    }
+
+    public static class ArgumentsTest extends ArgumentsAdapter {
+        @Parameter(names = "-r", required = true)
+        String req;
+
+        @Parameter(names = "-o")
+        int opt;
+
+        @Parameter(names = "-m", arity = 2)
+        int[] multiple;
+
+        @Parameter(names = { "-statusfil" },
+                converter = OptionalPathConverter.class,
+                validateValueWith = OptionalWritableFileValidator.class
+        )
+        Optional<Path> statusfil = Optional.empty();
+
+
+        @Parameter(names = "-e")
+        TestEnum enumverdi;
+    }
+
+    private static class ArgumentsAdapter implements Arguments{
+        @Override
+        public boolean hjelp() {
+            return false;
+        }
+    }
+
+    private enum TestEnum{
+        A, B
+    }
+}
