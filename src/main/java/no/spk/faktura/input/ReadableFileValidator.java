774f6cbd25e8e937a9d6313a151bb176fd5f3ed5
Snorre E. Brekke <snorre.brekke@spk.no>
2015-10-19 13:47:43 +0200

    POPKORN-190 - Validator for lesbar sti.

--- src/main/java/no/spk/faktura/input/ReadableFileValidator.java
+++ src/main/java/no/spk/faktura/input/ReadableFileValidator.java
@@ -9,28 +9,13 @@ import com.beust.jcommander.ParameterException;
 public class ReadableFileValidator implements IValueValidator<Path> {
     @Override
     public void validate(final String name, final Path value) throws ParameterException {
-        //Files.exists(value) viser seg å returnerer false i noen tilfeller  der file.exists returnerer true.
-        if (!value.toFile().exists()) {
-            throw new ParameterException(
-                    "Filen "
-                            + value
-                            + " eksisterer ikke, verifiser at du har angitt rett filsti."
-            );
-        }
+        new ReadablePathValidator().validate(name, value);
 
         if (!Files.isRegularFile(value)) {
             throw new ParameterException(
                     "Stien "
                             + value
                             + " peker ikke til en fil, verifiser at du har angitt rett filsti."
-            );
-        }
-
-        if (!Files.isReadable(value)) {
-            throw new ParameterException(
-                    "Filen "
-                            + value
-                            + " er ikke lesbar for batchen, verifiser at batchbrukeren har lesetilgang til filen."
             );
         }
     }
